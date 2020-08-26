be02adcabfdf1cdf71c91cd422f1b23107569d38
Rafiou Sitou <rafiousitou90@gmail.com>
2020-08-26 18:26:28 -0300

    feat(): Add SpamCheckerTest unit test.

---
+++ tests/Service/SpamCheckerTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Entity\Comment;
+use App\Service\SpamChecker;
+use DateTimeImmutable;
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Response\MockResponse;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+
+class SpamCheckerTest extends TestCase
+{
+    public function testSpamScoreWithInvalidRequest ()
+    {
+        $comment = $this->getComment();
+
+        $context = [];
+
+        $client = new MockHttpClient([
+                new MockResponse('invalid', ['response_headers' => ['x-akismet-debug-help: Invalid key']])
+            ]
+        );
+        $checker = new SpamChecker($client, 'bad_akismet_key');
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Unable to check for spam: invalid (Invalid key).');
+        $checker->getSpamScore($comment, $context);
+    }
+
+    /**
+     * @dataProvider getComments
+     *
+     * @param int $expectedScore
+     * @param ResponseInterface $response
+     * @param Comment $comment
+     * @param array $context
+     * @throws ClientExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
+     */
+    public function testSpamScore (int $expectedScore, ResponseInterface $response, Comment $comment, array $context)
+    {
+        $client = new MockHttpClient([$response]);
+        $checker = new SpamChecker($client, 'bad_akismet_key');
+        $score = $checker->getSpamScore($comment, $context);
+        $this->assertSame($expectedScore, $score);
+    }
+
+     public function getComments (): iterable
+     {
+         $comment = $this->getComment();
+         $context = [];
+
+         $response = new MockResponse('', ['response_headers' => ['x-akismet-pro-tip: discard']]);
+         yield 'blatant_spam' => [2, $response, $comment, $context];
+
+         $response = new MockResponse('true');
+         yield 'spam' => [1, $response, $comment, $context];
+
+         $response = new MockResponse('false');
+         yield 'ham' => [0, $response, $comment, $context];
+     }
+
+    private function getComment (): Comment
+    {
+        return (new Comment())
+            ->setAuthor('Author')
+            ->setEmail('author_email@domain.com')
+            ->setText('Comment content')
+            ->setCreatedAt(new DateTimeImmutable())
+        ;
+    }
+}
