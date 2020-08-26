174da109a20ddee84da3f9a5288fd9169191ff29
Rafiou Sitou <rafiousitou90@gmail.com>
2020-08-26 17:21:56 -0300

    feat(): Add Akismet SpamChecker service and generate secrets key AKISMET_KEY.

---
+++ src/Service/SpamChecker.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Comment;
+use http\Exception\RuntimeException;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class SpamChecker
+{
+    /**
+     * @var HttpClientInterface
+     */
+    private HttpClientInterface $httpClient;
+    private string $endpoint;
+
+    public function __construct (HttpClientInterface $httpClient, string $akismetKey)
+    {
+        $this->httpClient = $httpClient;
+        $this->endpoint = sprintf('https://%s.rest.akismet.com/1.1/comment-check', $akismetKey);
+    }
+
+    /**
+     * @param Comment $comment
+     * @param array $context
+     * @return int
+     * @throws ClientExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
+     */
+    public function getSpamScore (Comment $comment, array $context): int
+    {
+        $response = $this->httpClient->request('POST', $this->endpoint, [
+            'body' => array_merge($context, [
+                'blog' => 'https://guestbook.example.com',
+                'comment_type' => 'comment',
+                'comment_author' => $comment->getAuthor(),
+                'comment_author_email' => $comment->getEmail(),
+                'comment_content' => $comment->getText(),
+                'comment_date_gmt' => $comment->getCreatedAt()->format('c'),
+                'blog_lang' => 'en',
+                'blog_charset' => 'UTF-8',
+                'is_test' => true
+            ])
+        ]);
+
+        $headers = $response->getHeaders();
+        if ('discard' === ($headers['x-akismet-pro-tip'][0] ?? '')) {
+            return 2;
+        }
+
+        $content = $response->getContent();
+        if (isset($headers['x-akismet-debug-help'][0])) {
+            throw new RuntimeException(
+                sprintf('Unable to check for spam: %s (%s).', $content, $headers['x-akismet-debug-help'][0])
+            );
+        }
+
+        return  'true' === $content ? 1 : 0;
+    }
+}
