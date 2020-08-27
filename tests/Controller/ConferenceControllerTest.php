8b2d3e8139bf4abfa7213608a2c8224e194beecf
Rafiou Sitou <rafiousitou90@gmail.com>
2020-08-27 13:42:36 -0300

    feat(): Add testConferencePage in the ConferenceControllerTest.

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -14,4 +14,20 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Give your feedback!');
     }
+
+    public function testConferencePage ()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertCount(3, $crawler->filter('h4'));
+
+        $client->clickLink('View');
+//        $client->click($crawler->filter('h4 + p a')->link());
+
+        $this->assertPageTitleContains('Toronto');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Toronto 2020');
+        $this->assertSelectorExists('div:contains("There are 10 comments")');
+    }
 }
