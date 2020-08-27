409b27ffc317090ee5201363722a64159c5979c4
Rafiou Sitou <rafiousitou90@gmail.com>
2020-08-27 14:21:10 -0300

    feat(): Add testCommentSubmission in the ConferenceControllerTest.

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -2,14 +2,27 @@
 
 namespace App\Tests\Controller;
 
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ConferenceControllerTest extends WebTestCase
 {
+    /**
+     * @var KernelBrowser
+     */
+    protected KernelBrowser $client;
+
+    /**
+     * @return void
+     */
+    public function setUp (): void
+    {
+        $this->client = static::createClient();
+    }
+
     public function testIndex ()
     {
-        $client = static::createClient();
-        $client->request('GET', '/');
+        $this->client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Give your feedback!');
@@ -17,17 +30,32 @@ class ConferenceControllerTest extends WebTestCase
 
     public function testConferencePage ()
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $crawler = $this->client->request('GET', '/');
 
         $this->assertCount(3, $crawler->filter('h4'));
 
-        $client->clickLink('View');
+        $this->client->clickLink('View');
 //        $client->click($crawler->filter('h4 + p a')->link());
 
         $this->assertPageTitleContains('Toronto');
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Toronto 2020');
         $this->assertSelectorExists('div:contains("There are 10 comments")');
+    }
+
+    public function testCommentSubmission ()
+    {
+        $this->client->request('GET', '/conference/brasilia-2021');
+
+        $this->client->submitForm('Submit', [
+            'comment_form[author]' => 'Rafiou',
+            'comment_form[text]' => 'Some feedback from an automated functional test',
+            'comment_form[email]' => 'rafiou@domain.com',
+            'comment_form[photo]' => dirname(__DIR__, 2).'/public/images/under-construction.gif',
+        ]);
+
+        $this->assertResponseRedirects();
+        $this->client->followRedirect();
+        $this->assertSelectorExists('div:contains("There are 1 comments")');
     }
 }
