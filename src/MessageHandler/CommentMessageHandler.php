eb0408e8a03665e92d8a76521f05b5adcf73f1cb
Rafiou Sitou <rafiousitou90@gmail.com>
2020-08-28 21:01:00 -0300

    feat(): Add Admin Email Notification, and install twig/cssinliner-extra, and twig/inky-extra dependencies.

--- src/MessageHandler/CommentMessageHandler.php
+++ src/MessageHandler/CommentMessageHandler.php
@@ -7,6 +7,8 @@ use App\Repository\CommentRepository;
 use App\Service\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
+use Symfony\Bridge\Twig\Mime\NotificationEmail;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Workflow\WorkflowInterface;
@@ -37,6 +39,11 @@ class CommentMessageHandler implements MessageHandlerInterface
      * @var WorkflowInterface
      */
     private WorkflowInterface $workflow;
+    /**
+     * @var MailerInterface
+     */
+    private MailerInterface $mailer;
+    private string $adminEmail;
 
     public function __construct (
         EntityManagerInterface $entityManager,
@@ -44,6 +51,8 @@ class CommentMessageHandler implements MessageHandlerInterface
         CommentRepository $commentRepository,
         MessageBusInterface $bus,
         WorkflowInterface $commentStateMachine,
+        MailerInterface $mailer,
+        string $adminEmail,
         LoggerInterface $logger = null
     )
     {
@@ -52,6 +61,8 @@ class CommentMessageHandler implements MessageHandlerInterface
         $this->commentRepository = $commentRepository;
         $this->bus = $bus;
         $this->workflow = $commentStateMachine;
+        $this->mailer = $mailer;
+        $this->adminEmail = $adminEmail;
         $this->logger = $logger;
     }
 
@@ -81,6 +92,15 @@ class CommentMessageHandler implements MessageHandlerInterface
         } elseif ($this->workflow->can($comment, 'publish') || $this->workflow->can($comment, 'publish_ham')) {
             $this->workflow->apply($comment, $this->workflow->can($comment, 'publish') ? 'publish' : 'publish_ham');
             $this->entityManager->flush();
+
+            $this->mailer->send((new NotificationEmail())
+                    ->subject('New comment posted')
+                    ->htmlTemplate('emails/comment_notification.html.twig')
+                    ->from($this->adminEmail)
+                    ->to($this->adminEmail)
+                    ->context(['comment' => $comment])
+                )
+            ;
 
         } elseif ($this->logger) {
             $this->logger->debug('Dropping comment message', [
