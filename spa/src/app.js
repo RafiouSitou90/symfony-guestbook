0861a7ed284fe9b347b323ba4f4fce833b01a21e
Rafiou Sitou <rafiousitou90@gmail.com>
2020-09-01 21:19:24 -0300

    feat(): Styling the SPA.

--- spa/src/app.js
+++ spa/src/app.js
@@ -5,14 +5,22 @@ import Router, {Link} from "preact-router";
 import Home from "./pages/home";
 import Conference from "./pages/conference";
 
+import '../assets/css/app.scss';
+
 const App = () => {
     return (
         <div>
-            <header>
-                <Link href="/">Home</Link>
-                <br/>
-                <Link href="/conference/brasilia-2021">Brasilia 2021</Link>
+            <header className="header">
+                <nav className="navbar navbar-light bg-light">
+                    <div className="container">
+                        <Link className="navbar-brand mr-4 pr-2" href="/">&#128217; Guestbook</Link>
+                    </div>
+                </nav>
+                <nav className="bg-light border-bottom text-center">
+                    <Link className="nav-conference" href="/conference/brasilia-2021">Brasilia 2021</Link>
+                </nav>
             </header>
+
 
             <Router>
                 <Home path="/" />
